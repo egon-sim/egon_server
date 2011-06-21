0633f3c78941691d55eab268b97a478b181e94e5
Nikola Skoric <nskoric@gmail.com>
2011-06-21 17:46:39 +0200

    Rod controller now works - if you put it in auto, it maintains Tavg program.

--- src/es_rod_controller_server.erl
+++ src/es_rod_controller_server.erl
@@ -2,13 +2,14 @@
 -behaviour(gen_server).
 -import(es_convert).
 -export([start_link/0, init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
--record(rod_controller_state, {mode, speed}).
+-record(rod_controller_state, {mode, speed, manual_speed, ticks_per_second, ticks_left}).
 
 start_link() -> gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
 init([]) -> 
     gen_server:call(es_clock_server, {add_listener, ?MODULE}),
-    {ok, #rod_controller_state{speed=0}}.
+    Manual_speed = gen_server:call(es_curvebook_server, {get, pls, [speed_of_rods_in_manual]}),
+    {ok, #rod_controller_state{speed=0, manual_speed = Manual_speed, ticks_left = 0}}.
 
 handle_call({get, speed}, _From, State) ->
     {reply, State#rod_controller_state.speed, State};
@@ -16,20 +17,37 @@ handle_call({get, speed}, _From, State) ->
 handle_call({get, mode}, _From, State) ->
     {reply, State#rod_controller_state.mode, State};
 handle_call({set, mode, manual}, _From, State) ->
-    {reply, ok, State#rod_controller_state{mode=manual, speed=48}};
+    Speed = State#rod_controller_state.manual_speed,
+    {reply, ok, State#rod_controller_state{mode=manual, speed = Speed}};
 handle_call({set, mode, auto}, _From, State) ->
-    {reply, ok, State#rod_controller_state{mode=auto}};
+    Second_to_ticks = gen_server:call(es_clock_server, {get, seconds_to_ticks, 1}),
+    {reply, ok, State#rod_controller_state{mode=auto, ticks_per_second = Second_to_ticks}};
 
 handle_call({tick}, _From, State) when State#rod_controller_state.mode =:= manual ->
     {reply, tick, State};
 
 handle_call({tick}, _From, State) when State#rod_controller_state.mode =:= auto ->
-    Tavg = gen_server:call(es_core_server, {get, tavg}),
-    Tref = gen_server:call(es_w7300_server, {get, tref}),
-    Terr = Tref - Tavg,
-    Speed = State#rod_controller_state.speed,
-    New_speed = rod_speed(Terr, Speed),
-    {reply, tick, State#rod_controller_state{speed=New_speed}};
+    Terr = calc_terr(),
+
+    Old_speed = State#rod_controller_state.speed,
+    New_speed = rod_speed(Terr, Old_speed),
+
+    Ticks_left = State#rod_controller_state.ticks_left,
+    New_ticks = ticks_to_step(State),
+
+%    io:format("~p, ~p, ~p, ~p~n", [Terr, New_speed, Ticks_left, New_ticks]),
+
+    if
+        Ticks_left =< 1 ->
+	    step(New_speed),
+	    New_ticks_left = New_ticks
+        Ticks_left >= New_ticks ->
+	    New_ticks_left = New_ticks - 1;
+        Ticks_left > 1 ->
+	    New_ticks_left = Ticks_left - 1;
+    end,
+    New_state = State#rod_controller_state{speed=New_speed, ticks_left = New_ticks_left},
+    {reply, tick, New_state};
 
 handle_call(stop, _From, State) ->
     {stop, normal, stopped, State}.
@@ -44,11 +62,11 @@ rod_speed(Terr, Old_speed) ->
 %    io:format("~w ~w ~w~n", [Terr_F, Terr, Old_speed]),
     if
         Terr_F < -5 ->
-	    Speed = 72;
+	    Speed = -72;
         Terr_F =< -3 ->
 	    Speed = 32 * es_convert:c2f_delta(Terr) + 88;
         Terr_F =< -1.5 ->
-	    Speed = 8;
+	    Speed = -8;
         Terr_F =< -1 ->
 	    if
 	        Old_speed < 0 ->
@@ -73,3 +91,26 @@ rod_speed(Terr, Old_speed) ->
 	    Speed = 72
    end,
    Speed.
+
+step(Speed) ->
+    if
+        Speed < 0 ->
+    	    gen_server:call(es_rod_position_server, {action, step_in});
+        Speed > 0 ->
+    	    gen_server:call(es_rod_position_server, {action, step_out});
+	true ->
+	    speed_is_zero
+    end.
+
+ticks_to_step(State) when State#rod_controller_state.speed == 0 ->
+    0;
+ticks_to_step(State) ->
+    Speed = State#rod_controller_state.speed,
+    Seconds_to_step = 1 / (abs(Speed) / 60),
+    Ticks_per_second = State#rod_controller_state.ticks_per_second,
+    Ticks_per_second * Seconds_to_step.
+
+calc_terr() ->
+    Tavg = gen_server:call(es_core_server, {get, tavg}),
+    Tref = gen_server:call(es_w7300_server, {get, tref}),
+    Tref - Tavg.
