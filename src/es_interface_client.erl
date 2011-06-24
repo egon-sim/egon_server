019b54c5e2fb4c0d3419fd8910dd936460ace8d6
Nikola Skoric <nskoric@gmail.com>
2011-06-24 11:58:48 +0200

    Some more ideas.

--- src/es_interface_client.erl
+++ src/es_interface_client.erl
@@ -9,11 +9,25 @@ start() ->
     {ok,Sock} = gen_tcp:connect(Host,PortNo,[{active,false}, {packet,raw}]),
     Sock.
 
+send_without_recv(Sock, Message) ->
+    gen_tcp:send(Sock,Message),
+    stop(Sock),
+    {ok, A} = gen_tcp:recv(Sock, 0, 2000),
+    {match, [B]} = re:run(A, "^([^\\n]+)\\R*$", [{capture, [1], list}]),
+    B.
+
+send_with_close(Sock, Message) ->
+    gen_tcp:send(Sock,Message),
+    {ok, A} = gen_tcp:recv(Sock, 0, 2000),
+    stop(Sock),
+    {match, [B]} = re:run(A, "^([^\\n]+)\\R*$", [{capture, [1], list}]),
+    B.
+
 send(Sock, Message) ->
     gen_tcp:send(Sock,Message),
     {ok, A} = gen_tcp:recv(Sock, 0, 2000),
     {match, [B]} = re:run(A, "^([^\\n]+)\\R*$", [{capture, [1], list}]),
     B.
 
-stop({Sock}) ->
+stop(Sock) ->
     gen_tcp:close(Sock).
