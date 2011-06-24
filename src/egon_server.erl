4593da60ee62359d3b65db79d790be7aff9c9a77
Nikola Skoric <nskoric@gmail.com>
2011-06-24 13:00:17 +0200

    A few helper functions.

--- src/egon_server.erl
+++ src/egon_server.erl
@@ -1,8 +1,21 @@
 -module(egon_server).
 -compile(export_all).
 
+start() ->
+    application:start(egon_server).
+
+stop() ->
+    application:stop(egon_server).
+
+restart() ->
+    stop(),
+    start().
+
 run() ->
     gen_server:call(es_config_server, {unfreaze_sim}).
 
 pause() ->
     gen_server:call(es_config_server, {freaze_sim}).
+
+load() ->
+    gen_server:call(es_config_server, {load_config}).
