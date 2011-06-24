62053370bd5aa732f7c9717133d9f6ecbb501b74
Nikola Skoric <nskoric@gmail.com>
2011-06-24 12:57:48 +0200

    set_up_defaults moved to es_config_server, where it belongs.

--- src/es_app.erl
+++ src/es_app.erl
@@ -3,8 +3,7 @@
 -behaviour(application).
 -export([
    start/2,
-   stop/1,
-   set_up_defaults/0
+   stop/1
 ]).
 
 start(_Type, _StartArgs) ->
@@ -14,25 +13,7 @@ start(_Type, _StartArgs) ->
       Other ->
          Retval = {error, Other}
    end,
-   set_up_defaults(),
    Retval.
 
 stop(_State) ->
-   ok.
-
-set_up_defaults() ->
-    timer:sleep(500),
-    load_snapshot("priv/snapshots/full_power.snapshot"),
-    timer:sleep(500),
-    gen_server:call(es_config_server, {unfreaze_sim}),
-    ok.
-
-load_snapshot(Path) ->
-   {ok, [Snapshot]} = file:consult(Path),
-
-   lists:foreach(
-       fun({Server, Parm, Val}) ->
-       	   gen_server:call(Server, {set, Parm, Val})
-       end,
-       Snapshot),
    ok.
--- src/es_config_server.erl
+++ src/es_config_server.erl
@@ -9,6 +9,10 @@ start_link() -> gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 init([]) -> 
     {ok, #config_state{}}.
 
+handle_call({load_config}, _From, State) ->
+    set_up_defaults(),
+    {reply, ok, State};
+
 handle_call({freaze_sim}, _From, State) ->
     {reply, gen_server:call(es_clock_server, {stop_ticking}), State};
 
@@ -24,3 +28,21 @@ handle_info(_Info, State) -> {noreply, State}.
 terminate(_Reason, _State) -> ok.
 code_change(_OldVsn, State, _Extra) -> {ok, State}.
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+set_up_defaults() ->
+    timer:sleep(500),
+    load_snapshot("priv/snapshots/full_power.snapshot"),
+    timer:sleep(500),
+    gen_server:call(es_clock_server, {start_ticking}),
+    ok.
+
+load_snapshot(Path) ->
+   {ok, [Snapshot]} = file:consult(Path),
+
+   lists:foreach(
+       fun({Server, Parm, Val}) ->
+       	   gen_server:call(Server, {set, Parm, Val})
+       end,
+       Snapshot),
+   ok.
