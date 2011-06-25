10c442c242621282d747fe13044cbdb9be3704fa
Nikola Skoric <nskoric@gmail.com>
2011-06-25 13:01:43 +0200

    Helper function for generating compile.bat.

--- src/es_util.erl
+++ src/es_util.erl
@@ -2,7 +2,7 @@
 -import(eunit).
 -compile(export_all).
 
-run() ->
+run_tests() ->
     eunit:run(modules(), {suffix, "_test"}).
 
 modules() ->
@@ -11,3 +11,9 @@ modules() ->
 modules(App_file) ->
     {ok, [{application, _, [{description, _}, {vsn, _}, {modules, Modules}, {registered, _}, {applications, _}, {mod, _}]}]} = file:consult(App_file),
     Modules.
+
+generate_compile() ->
+    {ok, FD} = file:open("compile.bat", write),
+    lists:foreach(fun(M) -> io:format(FD, "erlc -o ebin src\\~p.erl~n", [M]) end, modules()),
+    file:close(FD),
+    ok.
