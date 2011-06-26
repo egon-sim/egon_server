a853ce45b906c6c2d23ba68c97a5a59cba62f998
Nikola Skoric <nskoric@gmail.com>
2011-06-26 10:59:12 +0200

    Rewriting interface client as gen_server.

--- src/es_interface_client.erl
+++ src/es_interface_client.erl
@@ -1,35 +1,65 @@
 -module(es_interface_client).
 -include_lib("include/es_common.hrl").
 -import(re).
+-behaviour(gen_server).
+-export([start_link/2, init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
+-record(client_state, {host, port, sock}).
 -compile(export_all).
 
-start() ->
-    start(1056).
+start_link(Host, Port) -> gen_server:start_link({local, ?MODULE}, ?MODULE, [Host, Port], []).
 
-start(PortNo) ->
-    Host = "localhost",
+init([Host, PortNo]) -> 
     {ok,Sock} = gen_tcp:connect(Host,PortNo,[{active,false}, {packet,raw}]),
-    Sock.
+    {ok, #client_state{sock = Sock, host = Host, port = PortNo}}.
 
-send_without_recv(Sock, Message) ->
-    gen_tcp:send(Sock,Message),
-    stop(Sock),
-    {ok, A} = gen_tcp:recv(Sock, 0, 2000),
-    {match, [B]} = re:run(A, "^([^\\n]+)\\R*$", [{capture, [1], list}]),
-    B.
-
-send_with_close(Sock, Message) ->
-    gen_tcp:send(Sock,Message),
-    {ok, A} = gen_tcp:recv(Sock, 0, 2000),
-    stop(Sock),
-    {match, [B]} = re:run(A, "^([^\\n]+)\\R*$", [{capture, [1], list}]),
-    B.
-
-send(Sock, Message) ->
+handle_call({send, Message}, _From, State) -> 
+    Sock = State#client_state.sock,
     gen_tcp:send(Sock,Message),
     {ok, A} = gen_tcp:recv(Sock, 0, 2000),
     {match, [B]} = re:run(A, "^([^\\n]+)\\R*$", [{capture, [1], list}]),
-    B.
+    {reply, B, State};
 
-stop(Sock) ->
-    gen_tcp:close(Sock).
+handle_call({switch_port, PortNo}, _From, State) -> 
+    Sock = State#client_state.sock,
+    gen_tcp:close(Sock),
+
+    Host = State#client_state.host,
+    {ok, New_sock} = gen_tcp:connect(Host,PortNo,[{active,false}, {packet,raw}]),
+    {reply, ok, State#client_state{sock = New_sock, port = PortNo}};
+
+handle_call(stop, _From, State) -> 
+    Sock = State#client_state.sock,
+    gen_tcp:close(Sock),
+    {stop, normal, State}.
+
+%handle_call(_Request, _From, State) -> {reply, ok, State}.
+handle_cast(_Msg, State) -> {noreply, State}.
+handle_info(_Info, State) -> {noreply, State}.
+terminate(_Reason, _State) -> ok.
+code_change(_OldVsn, State, _Extra) -> {ok, State}.
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+start() ->
+    start_link("localhost", 1055).
+
+new_sim() ->
+    {started, Port} = parse(send("{ask, start_new_simulator}")),
+    gen_server:call(?MODULE, {switch_port, Port}).
+
+send(Message) ->
+    gen_server:call(?MODULE, {send, Message}).
+
+stop() ->
+    gen_server:call(?MODULE, stop).
+
+
+parse(Buffer) ->
+    {match, [Tuple]} =  re:run(Buffer, "^\\W*({.*})\\W*$", [{capture, [1], list}]),
+%    io:format("~p~n", [Tuple]),
+    {ok, Tokens, _Line} = erl_scan:string("[" ++ Tuple ++ "]."),
+%    io:format("~p~n", [Tokens]),
+    {ok, [Args]} = erl_parse:parse_term(Tokens),
+    Args.
