e31005d0104837404a64e7fd9102ba1dd3e0efc9
Nikola Skoric <nskoric@gmail.com>
2011-07-19 11:12:58 +0200

    Updating test case.

--- src/egon_server.erl
+++ src/egon_server.erl
@@ -20,10 +20,10 @@ pause() ->
 
 general_test() ->
     ok = egon_server:start(),
-    {ok,_} = es_interface_client:start(),
-    ok = es_interface_client:new_sim(),
-    "305.0" = es_interface_client:send("{get, es_core_server, tavg}"),
-    "305.0" = es_interface_client:send("{get, es_w7300_server, tref}"),
-    "ok" = es_interface_client:send("{action, es_rod_position_server, step_in}"),
-    "304.9416710346633" = es_interface_client:send("{get, es_core_server, tavg}"),
+    {ok,_} = egon_client:start(),
+    ok = egon_client:new_sim(),
+    "305.0" = egon_client:send("{get, es_core_server, tavg}"),
+    "305.0" = egon_client:send("{get, es_w7300_server, tref}"),
+    "ok" = egon_client:send("{action, es_rod_position_server, step_in}"),
+    "304.9416710346633" = egon_client:send("{get, es_core_server, tavg}"),
     ok.
