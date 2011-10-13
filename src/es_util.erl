22b96fb69561e4a5f127f9d050b8116f644c54f6
Nikola Skoric <nskoric@gmail.com>
2011-10-14 00:38:33 +0200

    Removing unneeded function now that make works.

--- src/es_util.erl
+++ src/es_util.erl
@@ -1,7 +1,7 @@
 -module(es_util).
 -import(eunit).
 
--export([run_tests/0, generate_compile/0 , unit_test/0]).
+-export([run_tests/0, unit_test/0]).
 
 run_tests() ->
     {_Passed, Failed, _PassedList, FailedList} = eunit:run(modules(), {suffix, "_test"}),
@@ -15,12 +15,6 @@ modules() ->
 modules(App_file) ->
     {ok, [{application, _, [{description, _}, {vsn, _}, {modules, Modules}, {registered, _}, {applications, _}, {mod, _}, {env, _}]}]} = file:consult(App_file),
     Modules.
-
-generate_compile() ->
-    {ok, FD} = file:open("compile.bat", write),
-    lists:foreach(fun(M) -> io:format(FD, "erlc -o ebin src\\~p.erl~n", [M]) end, modules()),
-    file:close(FD),
-    ok.
 
 -include_lib("include/es_common.hrl").
 
