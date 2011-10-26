ea1bdf6e7f507a2761990622be258ee8e08d9e43
Nikola Skoric <nskoric@gmail.com>
2011-10-26 04:05:04 +0200

    Moving .rel file, making release generation a bit more portable.

--- src/es_releasing.erl
+++ src/es_releasing.erl
@@ -2,9 +2,9 @@
 -export([create_release/0, clear_files/0]).
 
 create_release() ->
-    create_release("release_files", "egon_server", "egon_release").
+    create_release("releases", "egon_server", "egon_server-0.1", "erts-5.6.4").
 
-create_release(Release_dir, Rel_file_name, Release_name) ->
+create_release(Release_dir, Rel_file_name, Release_name, Erts) ->
     Full_name = Release_dir ++ "/" ++ Rel_file_name,
     Full_release_name = Release_dir ++ "/" ++ Release_name,
     code:add_patha("./ebin"),
@@ -12,27 +12,27 @@ create_release(Release_dir, Rel_file_name, Release_name) ->
     systools:make_script(Full_name, []),
     systools:make_tar(Full_name, [{erts, code:root_dir()}]),
     erl_tar:extract(Full_name ++ ".tar.gz", [{cwd, Full_release_name}, compressed]),
-    generate_ini(Release_dir, Release_name),
-    generate_install(Release_dir, Release_name),
-    generate_start(Release_dir, Rel_file_name, Release_name),
+    generate_ini(Release_dir, Release_name, Erts),
+    generate_install(Release_dir, Release_name, Erts),
+    generate_start(Release_dir, Rel_file_name, Release_name, Erts),
     copy_files(Release_dir, Rel_file_name, Release_name),
     cleanup(Release_dir, Rel_file_name, Release_name),
     ok.
 
-generate_ini(Release_dir, Release_name) ->
+generate_ini(Release_dir, Release_name, Erts) ->
     {ok, Path} = file:get_cwd(),
     Filename = Release_dir ++ "/" ++ Release_name ++ "/erl.ini",
-    Content = "[erlang]\nBindir=" ++ Path ++ "/" ++ Release_dir ++ "/" ++ Release_name ++ "/erts-5.6.4/bin\nProgname=erl\nRootdir=" ++ Path ++ "/" ++ Release_dir ++ "/" ++ Release_name,
+    Content = "[erlang]\nBindir=" ++ Path ++ "/" ++ Release_dir ++ "/" ++ Release_name ++ "/" ++ Erts ++ "/bin\nProgname=erl\nRootdir=" ++ Path ++ "/" ++ Release_dir ++ "/" ++ Release_name,
     file:write_file(Filename, Content).
     
-generate_install(Release_dir, Release_name) ->
+generate_install(Release_dir, Release_name, Erts) ->
     Filename = Release_dir ++ "/" ++ Release_name ++ "/install.bat",
-    Content = "move erl.ini erts-5.6.4/bin/\n",
+    Content = "move erl.ini " ++ Erts ++ "/bin/\n",
     file:write_file(Filename, Content).    
 
-generate_start(Release_dir, Rel_file_name, Release_name) ->
+generate_start(Release_dir, Rel_file_name, Release_name, Erts) ->
     Filename = Release_dir ++ "/" ++ Release_name ++ "/start.bat",
-    Content = "erts-5.6.4\\bin\\erl -sname " ++ Rel_file_name ++ " -boot " ++ Rel_file_name ++"\n",
+    Content = Erts ++ "\\bin\\erl -sname " ++ Rel_file_name ++ " -boot " ++ Rel_file_name ++" -detached\n",
     file:write_file(Filename, Content).    
 
 clear_files() ->
