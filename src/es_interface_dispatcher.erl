728b2f6de109acc897ad3974eb0be8a28fc833a5
Nikola Skoric <nskoric@gmail.com>
2012-01-06 03:15:05 +0100

    Addind documentation.

--- src/es_interface_dispatcher.erl
+++ src/es_interface_dispatcher.erl
@@ -1,13 +1,52 @@
+%%%------------------------------------------------------------------
+%%% @author Nikola Skoric <nskoric@gmail.com>
+%%% @copyright 2011 Nikola Skoric
+%%% @doc Server starting and supervising TCP interfaces to outside
+%%%      world.
+%%% @end
+%%%------------------------------------------------------------------
 -module(es_interface_dispatcher).
 
 -behaviour(supervisor).
 -define(SERVER(SimId), {global, {SimId, ?MODULE}}).
 
--export([start_link/1, start_child/2, children/1, init/1]).
+%% API
+-export([
+	start_link/1,
+	start_child/2,
+	children/1
+	]).
 
+
+%% Supervisor callbacks
+-export([init/1]).
+
+
+%%%==================================================================
+%%% API
+%%%==================================================================
+
+%%-------------------------------------------------------------------
+%% @doc Starts the supervisor.
+%%
+%% @spec start_link(SimId::integer()) -> Result
+%% where
+%%  Result = {ok,Pid} | ignore | {error,Error}
+%%  Pid = pid()
+%%  Error = {already_started,Pid}} | shutdown | term()
+%% @end
+%%-------------------------------------------------------------------
 start_link(SimId) ->
     supervisor:start_link(?SERVER(SimId), ?MODULE, [SimId]).
 
+%%-------------------------------------------------------------------
+%% @doc Starts new interface server.
+%%
+%% @spec start_child(SimId, User) -> {ok, Port}
+%% where
+%%  Port = integer()
+%% @end
+%%-------------------------------------------------------------------
 start_child(SimId, User) ->
     io:format("Starting interface server.~n"),
     {ok, Child} = supervisor:start_child(?SERVER(SimId), [User]),
@@ -17,8 +56,23 @@ start_child(SimId, User) ->
     gen_server:cast(Child, {listen}),
     {ok, Port}.
 
+%%-------------------------------------------------------------------
+%% @doc Returns list of  simulator supervisor's children for given
+%%      simulator ID (in fact calls supervisor:which_children/1).
+%%
+%% @spec children(SimId) -> [{Id,Child,Type,Modules}]
+%% where
+%%  Child = pid() | undefined
+%%  Type = worker | supervisor
+%%  Modules = [atom()] | dynamic
+%% @end
+%%-------------------------------------------------------------------
 children(SimId) ->
     supervisor:which_children(?SERVER(SimId)).
+
+%%%==================================================================
+%%% supervisor callbacks
+%%%==================================================================
 
 init([SimId]) -> 
 %    io:format("Starting interface dispatcher.~n"),
